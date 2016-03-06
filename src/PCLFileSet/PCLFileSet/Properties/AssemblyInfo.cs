93629a8a0411f39c4d8589fed3fd37ebdeaa32e8
MarcBehnke <marc.behnke@gmail.com>
2016-03-06 16:08:04 -0600

    Created SharedAssemblyInfo.cs and linked to projects.

--- src/PCLFileSet/PCLFileSet/Properties/AssemblyInfo.cs
+++ src/PCLFileSet/PCLFileSet/Properties/AssemblyInfo.cs
@@ -3,27 +3,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("PCLFileSet")]
-[assembly: AssemblyDescription("File set from multiple inclusion/exclusion glob paths.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Marc Behnke")]
-[assembly: AssemblyProduct("PCLFileSet")]
-[assembly: AssemblyCopyright("Copyright © Marc Behnke 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+// See SharedAssemblyInfo.cs for compatible assembly attributes
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0")]
+[assembly: NeutralResourcesLanguage("en")]
