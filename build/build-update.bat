65e66a442c83ea63e844577e1e49d5c1ce856c04
MarcBehnke <marc.behnke@gmail.com>
2016-04-20 01:13:22 -0500

    Updated to nunit v3.2.1.

--- build/build-update.bat
+++ build/build-update.bat
@@ -1,13 +1,13 @@
 @echo off
 REM Updates packages in paket.dependencies to the latest version and updates paket.lock.
 
-paket\paket.bootstrapper.exe
+.paket\paket.bootstrapper.exe
 if errorlevel 1 (
     exit /b %errorlevel%
 )
 
 REM The -f parameter forces the update which is needed for HTTP downloads.
-paket\paket.exe update -f
+.paket\paket.exe update -f
 if errorlevel 1 (
     exit /b %errorlevel%
 )
